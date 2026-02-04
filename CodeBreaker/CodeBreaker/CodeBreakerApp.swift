87459ea94ad85e00a5a20bea42d4e9f4f4b124f7
Jonathan Duran-Ortiz <jonathandurantula35@gmail.com>
2026-02-04 16:47:26 -0500

    Initial Files

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Jonathan Duran-Ortiz on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
